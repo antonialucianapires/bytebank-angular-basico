88f5d411d395740e7149646c3b604f44dafd9c2a
Antonia Luciana Pires <antonialucianapires@gmail.com>
2021-07-09 02:35:49 -0300

    feat: primeiro componente -> form de transferencia

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+//Component é um Decorator que funciona como um metadata para adicionar propriedades às classes
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bytebank';
+}
