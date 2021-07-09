52d351a829a171f567ed4330f8d1c79f1c68e3eb
Antonia Luciana Pires <antonialucianapires@gmail.com>
2021-07-09 02:50:48 -0300

    feat: criando o evento do formulario

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,10 +1,13 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector:'app-nova-transferencia',
-  templateUrl:'./nova-transferencia.component.html',
-  styleUrls:['./nova-transferencia.component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
-
+  // tslint:disable-next-line:typedef
+  transferir() {
+    console.log('Solicitação realizada com sucesso');
+  }
 }
